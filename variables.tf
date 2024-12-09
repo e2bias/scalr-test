df73c23d5782104af3373f474a98b9036a24b1c8
teberhar <tobias_eberhartinger@bmc.com>
2024-12-09 14:54:59 +0100

    Testing...

--- variables.tf
+++ variables.tf
@@ -1,36 +1,29 @@
 variable "account_id" {
   type = string
-  default = null
 }
 
 variable "display_name" {
   type = string
-  default = null
 }
 
 variable "name" {
   type = string
-  default = "my-instance"
 }
 
 variable "machine_type" {
   type = string
-  default = "n2-standard-2"
 }
 
 variable "zone" {
   type = string
-  default = "us-central1-a"
 }
 
 variable "image" {
   type = string
-  default = "debian-cloud/debian-11"
 }
 
 variable "network" {
   type = string
-  default = "default"
 }
 
 
